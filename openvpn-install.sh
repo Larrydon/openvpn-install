bb9ceebbcb11c009557a8701f2884575b91adb7c
Larrydon <larrydon2008@gmail.com>
2025-07-30 10:15:45 +0800

    Update openvpn-install.sh
    keepalive change to 60

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -780,7 +780,7 @@ user nobody
 group $NOGROUP
 persist-key
 persist-tun
-keepalive 10 120
+keepalive 10 60
 topology subnet
 server 10.8.0.0 255.255.255.0
 ifconfig-pool-persist ipp.txt" >>/etc/openvpn/server.conf
